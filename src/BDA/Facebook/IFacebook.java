79966540b05216373490f480a10b220bdabcbb66
hugo <hugo@PC-Hugo>
2018-10-15 23:03:48 +0100

    Adicionei alteração de janelas com o clique e iniciei a interface do fb

---
+++ src/BDA/Facebook/IFacebook.java
@@ -0,0 +1,22 @@
+package BDA.Facebook;
+
+public interface IFacebook {
+
+	public void createAccount();
+	
+	public void logIn();
+	
+	public void logOut();
+	
+	public void changeConfig();
+	
+	public void createPost();
+	
+	public void getTimeLine();
+	
+	public void setFilter();
+	
+	public void getMessages();
+	
+	public void sendMessage();
+}
