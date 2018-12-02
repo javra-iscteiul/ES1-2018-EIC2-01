0d6568ce2282ace00b258b64635c0c9f40ed3c43
Ana Nunes <arsns@iscte-iul.pt>
2018-12-02 17:57:03 +0000

    timeline remover/adicionar serviços

--- src/BDA/ControllerBDA.java
+++ src/BDA/ControllerBDA.java
@@ -1,9 +1,11 @@
 package BDA;
 
 import org.w3c.dom.Node;
+import javafx.scene.input.MouseEvent;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -15,6 +17,15 @@ public class ControllerBDA {
 	@FXML
 	private ListView<Mensagem> list;
 	
+	
+	@FXML
+	private CheckBox email;
+	
+	@FXML
+	private CheckBox facebook;
+	
+	@FXML
+	private CheckBox twitter;
 	
 	/**
 	 * TextField correspondente à palavra ou frase a pesquisar
@@ -33,12 +44,91 @@ public class ControllerBDA {
 	}
 	
 	/**
-	 * Procedimento que filtra a lista de emails dada uma palavra ou frase
+	 * Procedimento que devolve a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void getMensagens(MouseEvent event) {
+		list.setItems(bda.getTimeLine());
+	}
+	
+	/**
+	 * Procedimento que filtra a lista de mensagens dada uma palavra ou frase
 	 * @param event
 	 */
 	@FXML
 	private void filter(ActionEvent event) {
 		System.out.println(pesquisa.getText());
 		list.setItems(bda.setFilter(pesquisa.getText()));
+	}
+	
+	/**
+	 * Procedimento que filtra a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void email(ActionEvent event) {
+		if(email.isSelected()){
+			bda.setShowEmail(true);
+		}
+		if(!email.isSelected()){
+			bda.setShowEmail(false);
+		}
+		list.setItems(bda.getTimeLine());
+	}
+	/**
+	 * Procedimento que filtra a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void facebook(ActionEvent event) {
+		if(facebook.isSelected()){
+			bda.setShowFacebook(true);
+		}
+		if(!facebook.isSelected()){
+			bda.setShowFacebook(false);
+		}
+		list.setItems(bda.getTimeLine());
+	}
+	/**
+	 * Procedimento que filtra a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void twitter(ActionEvent event) {
+		if(twitter.isSelected()){
+			bda.setShowTwitter(true);
+		}
+		if(!twitter.isSelected()){
+			bda.setShowTwitter(false);
+		}
+		list.setItems(bda.getTimeLine());
+	}
+	
+	/**
+	 * Procedimento que filtra a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void onlyEmail(ActionEvent event) {
+		list.setItems(bda.getOnlyEmail());
+	}
+	
+	/**
+	 * Procedimento que filtra a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void onlyFacebook(ActionEvent event) {
+		list.setItems(bda.getOnlyFacebook());
+	}
+	
+	/**
+	 * Procedimento que filtra a lista de mensagens
+	 * @param event
+	 */
+	@FXML
+	private void onlyTwitter(ActionEvent event) {
+		list.setItems(bda.getOnlyTwitter());
 	}
 }
--- src/BDA/TimelineBDA.java
+++ src/BDA/TimelineBDA.java
@@ -13,29 +13,47 @@ public class TimelineBDA {
 	 */
 	private ObservableList<Mensagem> mensagens = FXCollections.observableArrayList();
 	
+	private boolean showEmail =true;
+	private boolean showFacebook =true;
+	private boolean showTwitter =true;
+
+	
+	
 	IService email= new Email();
 	IService facebook= new Facebook();
 	IService twitter= new App_twitter(); 
 	
 	TimelineBDA() {
-		Credential emailCred = new Credential(XMLclass.getLogin(XMLclass.configFile, XMLclass.emailService).getAttributes());
-		email.init(emailCred);
-		Credential facebookCred = new Credential(XMLclass.getLogin(XMLclass.configFile, XMLclass.facebookService).getAttributes());
-		facebook.init(facebookCred);
-		Credential twitterCred = new Credential(XMLclass.getLogin(XMLclass.configFile, XMLclass.twitterService).getAttributes());
-		twitter.init(twitterCred);
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.emailService) && showEmail) {
+			Credential emailCred = new Credential(XMLclass.getLogin(XMLclass.configFile, XMLclass.emailService).getAttributes());
+			email.init(emailCred);
+		}
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.facebookService) && showFacebook) {
+			Credential facebookCred = new Credential(XMLclass.getLogin(XMLclass.configFile, XMLclass.facebookService).getAttributes());
+			facebook.init(facebookCred);
+		}
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.twitterService) && showTwitter) {
+			Credential twitterCred = new Credential(XMLclass.getLogin(XMLclass.configFile, XMLclass.twitterService).getAttributes());
+			twitter.init(twitterCred);
+		}
 	}
 	
 	public ObservableList<Mensagem> getTimeLine(){
-		
-		for (Mensagem m: email.getTimeLine()){
-			mensagens.add(m);
+		mensagens.clear();
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.emailService) && showEmail) {
+			for (Mensagem m: email.getTimeLine()){
+				mensagens.add(m);
+			}
 		}
-		for (Mensagem m: facebook.getTimeLine()){
-			mensagens.add(m);
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.facebookService) && showFacebook) {
+			for (Mensagem m: facebook.getTimeLine()){
+				mensagens.add(m);
+			}
 		}
-		for (Mensagem m: twitter.getTimeLine()){
-			mensagens.add(m);
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.twitterService) && showTwitter) {
+			for (Mensagem m: twitter.getTimeLine()){
+				mensagens.add(m);
+			}
 		}
 		return mensagens;
 		
@@ -43,16 +61,80 @@ public class TimelineBDA {
 	
 	public ObservableList<Mensagem> setFilter(String s){
 		ObservableList<Mensagem> nova= FXCollections.observableArrayList();
-		for (Mensagem m: email.setFilter(s)){
-			nova.add(m);
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.emailService) && showEmail) {
+			for (Mensagem m: email.setFilter(s)){
+				nova.add(m);
+			}
 		}
-		for (Mensagem m: facebook.setFilter(s)){
-			nova.add(m);
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.facebookService) && showFacebook) {
+			for (Mensagem m: facebook.setFilter(s)){
+				nova.add(m);
+			}
 		}
-		for (Mensagem m: twitter.setFilter(s)){
-			nova.add(m);
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.twitterService) && showTwitter) {
+			for (Mensagem m: twitter.setFilter(s)){
+				nova.add(m);
+			}
 		}
 		return nova;
 	}
+	
+	public ObservableList<Mensagem> getOnlyTwitter(){
+		mensagens.clear();
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.twitterService) && showTwitter) {
+			for (Mensagem m: twitter.getTimeLine()){
+				mensagens.add(m);
+			}
+		}
+		return mensagens;
+		
+	}
+	
+	public ObservableList<Mensagem> getOnlyEmail(){
+		mensagens.clear();
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.emailService) && showEmail) {
+			for (Mensagem m: email.getTimeLine()){
+				mensagens.add(m);
+			}
+		}
+		return mensagens;
+		
+	}
+	
+	public ObservableList<Mensagem> getOnlyFacebook(){
+		mensagens.clear();
+		if (XMLclass.existsLogin(XMLclass.configFile, XMLclass.facebookService) && showFacebook) {
+			for (Mensagem m: facebook.getTimeLine()){
+				mensagens.add(m);
+			}
+		}
+		return mensagens;		
+	}
+
+	public boolean isShowEmail() {
+		return showEmail;
+	}
+
+	public void setShowEmail(boolean showEmail) {
+		this.showEmail = showEmail;
+	}
+
+	public boolean isShowFacebook() {
+		return showFacebook;
+	}
+
+	public void setShowFacebook(boolean showFacebook) {
+		this.showFacebook = showFacebook;
+	}
+
+	public boolean isShowTwitter() {
+		return showTwitter;
+	}
+
+	public void setShowTwitter(boolean showTwitter) {
+		this.showTwitter = showTwitter;
+	}
+	
+	
 }
 
